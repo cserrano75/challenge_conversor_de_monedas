ea88410f445dc6316fe68856ad7375a4a3c78c4b
Claudio Serrano <claudio.serrano.rojas@gmail.com>
2025-05-10 15:04:11 -0400

    Generando opciones del menu principal

--- Challenge_Conversion_De_Monedas_API/src/Principal.java
+++ Challenge_Conversion_De_Monedas_API/src/Principal.java
@@ -1,5 +1,21 @@
 public class Principal {
     public static void main(String[] args) {
+        System.out.println("*********************************************************");
+        System.out.println("Bienvenido(a) al conversor de monedas!! " +"\n" +
+                "Ingrese la opción del menu, según la conversión que desea realizar: " +"\n");
+
+        System.out.println("1) Dolar =>> peso argentino");
+        System.out.println("2) Peso argentino =>> dolar");
+        System.out.println("3) Dolar =>> Real brasileño");
+        System.out.println("4) Real brasileño =>> dolar");
+        System.out.println("5) Dolar =>> peso chileno");
+        System.out.println("6) Peso chileno =>> dolar");
+        System.out.println("7) Salir" +"\n");
+        System.out.println("*********************************************************");
+        System.out.println("Elija una opción válida:");
+
+
+
 
     }
 }
